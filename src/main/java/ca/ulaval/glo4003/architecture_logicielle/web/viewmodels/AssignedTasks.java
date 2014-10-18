e993316b38c732f156f180194367db364102e7b8
Jean-Philippe Gagné Guay <jean-philippe.gagne-guay.1@ulaval.ca>
2014-10-17 21:38:10 -0400

    Correction dans EmployeeViewModel.java et AssignedTasks.java

--- src/main/java/ca/ulaval/glo4003/architecture_logicielle/web/viewmodels/AssignedTasks.java
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/web/viewmodels/AssignedTasks.java
@@ -1,10 +1,10 @@
 package ca.ulaval.glo4003.architecture_logicielle.web.viewmodels;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class AssignedTasks {
-	
-	public List<String> tasks;
+	public List<String> tasks = new ArrayList<String>();
 
 	public List<String> getTasks() {
 		return tasks;
--- src/main/java/ca/ulaval/glo4003/architecture_logicielle/web/viewmodels/EmployeeViewModel.java
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/web/viewmodels/EmployeeViewModel.java
@@ -1,12 +1,13 @@
 package ca.ulaval.glo4003.architecture_logicielle.web.viewmodels;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class EmployeeViewModel {
 
 	public String email;
 	public String name;
-	public List<String> tasks;
+	public List<String> tasks = new ArrayList<String>();
 	
 	public String getEmail() {
 		return email;
