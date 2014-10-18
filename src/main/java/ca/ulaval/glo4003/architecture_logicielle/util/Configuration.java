7a471fc523e333c2337f54e09755900b99d1ffad
firebones <quentin.salmeron@gmail.com>
2014-10-17 20:51:59 -0400

    Ajout de la classe Configuration qui lit le fichier config.txt (dans database, à modifier au pire), et garde les informations dans un Map. Pattern du fichier : Nom du paramètre + "," + valeur. La valeur est récupérée en String avec la méthode getConfig(String key).

---
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/util/Configuration.java
@@ -0,0 +1,50 @@
+package ca.ulaval.glo4003.architecture_logicielle.util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Configuration {
+	private static Map<String, String> configs;
+	private static String configFile = "database/config.txt";
+
+	private Configuration() {
+	}
+
+	public static String getConfig(String key) {
+		if (configs == null) {
+			configs = new HashMap<String, String>();
+			loadConfig();
+		}
+		return configs.get(key);
+	}
+
+	private static void loadConfig() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(
+					configFile));
+			String line = reader.readLine();
+			while (line != null) {
+				parseToMap(line);
+				line = reader.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void parseToMap(String line) {
+		if (line.length() != 0) {
+			StringTokenizer tokenizer = new StringTokenizer(line, ",");
+			String configName = tokenizer.nextToken();
+			String configValue = tokenizer.nextToken();
+			configs.put(configName, configValue);
+		}
+	}
+}
