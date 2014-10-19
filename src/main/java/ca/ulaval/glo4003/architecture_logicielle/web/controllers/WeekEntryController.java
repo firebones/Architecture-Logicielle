f75dbcaf3e7488eb036e7cf97c9e49c0587abc9c
RomainDAV <romain.daversa@gmail.com>
2014-10-19 18:55:40 +0200

    Changement pour l'userId
    Utilisation de spring security Auth pour récup l'userId

--- src/main/java/ca/ulaval/glo4003/architecture_logicielle/web/controllers/WeekEntryController.java
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/web/controllers/WeekEntryController.java
@@ -6,6 +6,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import ca.ulaval.glo4003.architecture_logicielle.web.converters.WeekEntryConverter;
 import ca.ulaval.glo4003.architecture_logicielle.appConfig.AppConfiguration;
+import ca.ulaval.glo4003.architecture_logicielle.model.EmployeeEntry;
 import ca.ulaval.glo4003.architecture_logicielle.model.WeekEntry;
 import ca.ulaval.glo4003.architecture_logicielle.web.viewmodels.AssignedExpenses;
 import ca.ulaval.glo4003.architecture_logicielle.web.viewmodels.AssignedKilometers;
@@ -29,6 +32,8 @@ public class WeekEntryController {
 	
 	@RequestMapping(value = "/vehicleExpenses", method = RequestMethod.GET)
 	  public String getEnterTransportion(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
+		String userId = ((EmployeeEntry) auth.getPrincipal()).getEmail();
 	
 		// TODO: Pour le moment, c'est la date en cours qui d�termine la semaine � obtenir et le num�ro de semaine.
 		// TODO: �ventuellement, selon la s�rie hebdomadaire qui sera trait�e (le user aura plusieurs s�ries), le bon weekNumber sera pass�.
@@ -39,7 +44,7 @@ public class WeekEntryController {
 		List<String> datesOfWeek = getDaysOfWeek();
 		
 		// TODO : trouver comment r�cup�rer le email du user logg�
-		List<Integer> kilometersOfWeek = configuration.getWeekEntryByEmailAndWeek("joe@gmail.com", "41").getKilometersEntries();
+		List<Integer> kilometersOfWeek = configuration.getWeekEntryByEmailAndWeek(userId, "41").getKilometersEntries();
 		
 	     model.addAttribute("daysOfWeek", daysOfWeek);
 	     model.addAttribute("daysNameOfWeek", datesOfWeek);
@@ -55,9 +60,11 @@ public class WeekEntryController {
 	// TODO : ne fonctionne pas correctement. Je ne peux pas r�cup�rer la liste du formulaire. Pourtant je vois ma liste dans Firebug.
 	@RequestMapping(value = "/vehicleExpenses", method = RequestMethod.POST)
 	public String getValuesOfWeek(@ModelAttribute("assignedKilometers") AssignedKilometers assignedKilometers) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
+		String userId = ((EmployeeEntry) auth.getPrincipal()).getEmail();
 	
 		// TODO : remplacer les valeurs hardcod� par le user en cours, et �ventuellement, la s�rie hebdomadaire trait�e.
-		WeekEntry weekEntry = configuration.getWeekEntryByEmailAndWeek("joe@gmail.com", "41");
+		WeekEntry weekEntry = configuration.getWeekEntryByEmailAndWeek(userId, "41");
 		List<Integer> test = assignedKilometers.getKilometers();
 		weekEntry.setKilometersEntries(test);
 		
@@ -66,6 +73,8 @@ public class WeekEntryController {
 	
 	@RequestMapping(value = "/employeeExpenses", method = RequestMethod.GET)
 	  public String getEnterExpenses(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
+		String userId = ((EmployeeEntry) auth.getPrincipal()).getEmail();
 	
 		// TODO: Pour le moment, c'est la date en cours qui d�termine la semaine � obtenir et le num�ro de semaine.
 		// TODO: �ventuellement, selon la s�rie hebdomadaire qui sera trait�e (le user aura plusieurs s�ries), le bon weekNumber sera pass�.
@@ -76,7 +85,7 @@ public class WeekEntryController {
 		List<String> datesOfWeek = getDaysOfWeek();
 		
 		// TODO : trouver comment r�cup�rer le email du user logg�
-		List<Double> expensesOfWeek = configuration.getWeekEntryByEmailAndWeek("joe@gmail.com", "41").getEmployeeExpensesEntries();
+		List<Double> expensesOfWeek = configuration.getWeekEntryByEmailAndWeek(userId, "41").getEmployeeExpensesEntries();
 		
 	     model.addAttribute("daysOfWeek", daysOfWeek);
 	     model.addAttribute("daysNameOfWeek", datesOfWeek);
@@ -92,9 +101,11 @@ public class WeekEntryController {
 	// TODO : ne fonctionne pas correctement. Je ne peux pas r�cup�rer la liste du formulaire. Pourtant je vois ma liste dans Firebug.
 	@RequestMapping(value = "/employeeExpenses", method = RequestMethod.POST)
 	public String getValuesOfWeek(@ModelAttribute("assignedExpenses") AssignedExpenses assignedExpenses) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
+		String userId = ((EmployeeEntry) auth.getPrincipal()).getEmail();
 	
 		// TODO : remplacer les valeurs hardcod� par le user en cours, et �ventuellement, la s�rie hebdomadaire trait�e.
-		WeekEntry weekEntry = configuration.getWeekEntryByEmailAndWeek("joe@gmail.com", "41");
+		WeekEntry weekEntry = configuration.getWeekEntryByEmailAndWeek(userId, "41");
 		List<Double> test = assignedExpenses.getExpenses();
 		weekEntry.setEmployeeExpensesEntries(test);
 		
