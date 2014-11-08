e66a4b0bdf5dd0930fe5a4e5474b7f743f054a17
sebasbravo <sebasbravo@gmail.com>
2014-11-08 18:50:08 -0500

    javaMail

---
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/util/SendMail.java
@@ -0,0 +1,16 @@
+package ca.ulaval.glo4003.architecture_logicielle.util;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+	
+	
+	
+
+}
