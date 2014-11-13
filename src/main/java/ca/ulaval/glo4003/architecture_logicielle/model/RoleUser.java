90a07a894188998950ff18edc7f23866239430dc
tahyre <pape-tahyre.fall.1@ulaval.ca>
2014-11-13 17:25:18 -0500

    correction de l'enum de role

--- src/main/java/ca/ulaval/glo4003/architecture_logicielle/model/RoleUser.java
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/model/RoleUser.java
@@ -3,10 +3,10 @@ package ca.ulaval.glo4003.architecture_logicielle.model;
 public enum RoleUser
 {
 
-	EMPLOYEE("STATE_APPROVED"),
-	MANAGER("STATE_SUBMITTED"), 
-	COMPANY("STATE_INPROGRESS"), 
-	ADMIN("STATE_REFUSED");
+	EMPLOYEE("ROLE_EMPLOYEE"),
+	MANAGER("ROLE_MANAGER"), 
+	COMPANY("ROLE_COMPANY"), 
+	ADMIN("ROLE_ADMIN");
 
 	private final String roleString;
 
