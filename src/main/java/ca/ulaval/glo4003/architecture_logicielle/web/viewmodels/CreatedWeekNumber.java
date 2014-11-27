c0d4c60e68143fcfe6a4fb24fdcd944e65959103
Quentin <salmeron.quentin@etu.unice.fr>
2014-11-27 12:02:35 -0500

    Ajout du d'un modèle

---
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/web/viewmodels/CreatedWeekNumber.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo4003.architecture_logicielle.web.viewmodels;
+
+public class CreatedWeekNumber {
+	private Integer weekNumber;
+	
+	public Integer getWeekNumber() {
+		return weekNumber;
+	}
+	
+	public void setWeekNumber(Integer week) {
+		weekNumber = week;
+	}
+}
