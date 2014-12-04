e1cea169bd480fdf479cac46bda93ee4a249e8c7
Quentin <salmeron.quentin@etu.unice.fr>
2014-12-04 14:26:37 -0500

    Suppression de l'ajout automatique de la semaine 41.

--- src/main/java/ca/ulaval/glo4003/architecture_logicielle/dao/UserRepositoryImpl.java
+++ src/main/java/ca/ulaval/glo4003/architecture_logicielle/dao/UserRepositoryImpl.java
@@ -5,56 +5,54 @@ import java.util.List;
 
 import ca.ulaval.glo4003.architecture_logicielle.model.AbstractFactory;
 import ca.ulaval.glo4003.architecture_logicielle.model.EmployeeEntry;
-import ca.ulaval.glo4003.architecture_logicielle.model.PeriodPayBuilder;
-import ca.ulaval.glo4003.architecture_logicielle.model.PeriodPayBuilderImpl;
 import ca.ulaval.glo4003.architecture_logicielle.model.ProjectRepository;
 import ca.ulaval.glo4003.architecture_logicielle.model.RoleUser;
-import ca.ulaval.glo4003.architecture_logicielle.model.StateWeekEntry;
 import ca.ulaval.glo4003.architecture_logicielle.model.TaskEntry;
 import ca.ulaval.glo4003.architecture_logicielle.model.UserEntry;
 import ca.ulaval.glo4003.architecture_logicielle.model.UserRepository;
 import ca.ulaval.glo4003.architecture_logicielle.model.WeekEntry;
 import ca.ulaval.glo4003.architecture_logicielle.model.WeekEntryRepository;
 
-public class UserRepositoryImpl implements UserRepository
-{
-	XMLUserPersistance xmluserpersistance; 
-	
-	public UserRepositoryImpl(){
-		
+public class UserRepositoryImpl implements UserRepository {
+	XMLUserPersistance xmluserpersistance;
+
+	public UserRepositoryImpl() {
+
 		xmluserpersistance = XMLUserPersistance.getInstance();
 	}
 
-	
-	
 	@Override
-	public ArrayList<UserEntry> getAllUsers()
-	{
+	public ArrayList<UserEntry> getAllUsers() {
 		ArrayList<UserEntry> users = new ArrayList<UserEntry>();
-		
-		ArrayList<ArrayList<String>> userlist = xmluserpersistance.getAllUsers();
-		
-		for(ArrayList<String> tabuser: userlist){
-			
-			AbstractFactory factory = AbstractFactory.createFactory(tabuser.get(2));
+
+		ArrayList<ArrayList<String>> userlist = xmluserpersistance
+				.getAllUsers();
+
+		for (ArrayList<String> tabuser : userlist) {
+
+			AbstractFactory factory = AbstractFactory.createFactory(tabuser
+					.get(2));
 			UserEntry user = factory.createUser();
 			user.setName(tabuser.get(0));
 			user.setEmail(tabuser.get(1));
 			user.setHashedPassword(tabuser.get(3));
-			
-			if(tabuser.size() > 7 && (user.getRole() == RoleUser.EMPLOYEE || user.getRole() == RoleUser.MANAGER)){
-				((EmployeeEntry)user).setCompany(tabuser.get(4));
-				((EmployeeEntry)user).setDepartment(tabuser.get(5));
-				((EmployeeEntry)user).setRateHour(Double.parseDouble(tabuser.get(6)));
-				int j=7;
-				do{
+
+			if (tabuser.size() > 7
+					&& (user.getRole() == RoleUser.EMPLOYEE || user.getRole() == RoleUser.MANAGER)) {
+				((EmployeeEntry) user).setCompany(tabuser.get(4));
+				((EmployeeEntry) user).setDepartment(tabuser.get(5));
+				((EmployeeEntry) user).setRateHour(Double.parseDouble(tabuser
+						.get(6)));
+				int j = 7;
+				do {
 					ProjectRepository projects = new ProjectRepositoryImpl();
-					TaskEntry task = projects.getTaskById(Integer.parseInt(tabuser.get(j)));
+					TaskEntry task = projects.getTaskById(Integer
+							.parseInt(tabuser.get(j)));
 					((EmployeeEntry) user).assignTask(task);
 					j++;
-				}while(j<tabuser.size());
+				} while (j < tabuser.size());
 			}
-			
+
 			users.add(user);
 		}
 
@@ -62,50 +60,53 @@ public class UserRepositoryImpl implements UserRepository
 	}
 
 	@Override
-	public ArrayList<EmployeeEntry> getAllEmployees()
-	{
+	public ArrayList<EmployeeEntry> getAllEmployees() {
 		ArrayList<EmployeeEntry> users = new ArrayList<EmployeeEntry>();
-		
-		ArrayList<ArrayList<String>> userlist = xmluserpersistance.getAllUsers();
-		
-		for(ArrayList<String> tabuser: userlist){
-			
-			AbstractFactory factory = AbstractFactory.createFactory(tabuser.get(2));
+
+		ArrayList<ArrayList<String>> userlist = xmluserpersistance
+				.getAllUsers();
+
+		for (ArrayList<String> tabuser : userlist) {
+
+			AbstractFactory factory = AbstractFactory.createFactory(tabuser
+					.get(2));
 			UserEntry user = factory.createUser();
 			user.setName(tabuser.get(0));
 			user.setEmail(tabuser.get(1));
 			user.setHashedPassword(tabuser.get(3));
 
-			if(user.getRole() == RoleUser.EMPLOYEE || user.getRole() == RoleUser.MANAGER){
-				
-				((EmployeeEntry)user).setCompany(tabuser.get(4));
-				((EmployeeEntry)user).setDepartment(tabuser.get(5));
-				((EmployeeEntry)user).setRateHour(Double.parseDouble(tabuser.get(6)));
-				
-				if(tabuser.size() > 7){
-					
-					int j=7;
-					
-					do{
-						
+			if (user.getRole() == RoleUser.EMPLOYEE
+					|| user.getRole() == RoleUser.MANAGER) {
+
+				((EmployeeEntry) user).setCompany(tabuser.get(4));
+				((EmployeeEntry) user).setDepartment(tabuser.get(5));
+				((EmployeeEntry) user).setRateHour(Double.parseDouble(tabuser
+						.get(6)));
+
+				if (tabuser.size() > 7) {
+
+					int j = 7;
+
+					do {
+
 						ProjectRepository projects = new ProjectRepositoryImpl();
-						TaskEntry task = projects.getTaskById(Integer.parseInt(tabuser.get(j)));
+						TaskEntry task = projects.getTaskById(Integer
+								.parseInt(tabuser.get(j)));
 						((EmployeeEntry) user).assignTask(task);
 						j++;
-					}while(j<tabuser.size());
+					} while (j < tabuser.size());
 				}
 			}
-			
-			users.add((EmployeeEntry)user);
-			
+
+			users.add((EmployeeEntry) user);
+
 		}
 
 		return users;
 	}
 
 	@Override
-	public UserEntry getUserByEmail(String email)
-	{
+	public UserEntry getUserByEmail(String email) {
 		ArrayList<UserEntry> users = getAllUsers();
 		for (int i = 0; i < users.size(); i++) {
 			if (users.get(i).getEmail().equals(email)) {
@@ -116,39 +117,19 @@ public class UserRepositoryImpl implements UserRepository
 	}
 
 	@Override
-	public void addUser(UserEntry user)
-	{
-		if (getUserByEmail(user.getEmail()) == null){
-			
+	public void addUser(UserEntry user) {
+		if (getUserByEmail(user.getEmail()) == null) {
+
 			ArrayList<String> userelement = getUserString(user);
 			xmluserpersistance.addUser(userelement);
-			
-			WeekEntryRepository repositoryWeekEntry = new WeekEntryRepositoryImpl();
-			PeriodPayBuilder builderdefaultWeekEntry = new PeriodPayBuilderImpl();
-			builderdefaultWeekEntry.setInformation(user.getEmail(), 41, 2014, StateWeekEntry.INPROGRESS);
-			List<Integer> kilometersEntries = new ArrayList<Integer>();
-			
-			kilometersEntries.add(0);kilometersEntries.add(0);kilometersEntries.add(0);kilometersEntries.add(0);kilometersEntries.add(0);kilometersEntries.add(0);kilometersEntries.add(0);
-			builderdefaultWeekEntry.setEmployeeKilometers(kilometersEntries);
-			List<Double> employeeExpensesEntries = new ArrayList<Double>();
-			
-			employeeExpensesEntries.add(0.0);employeeExpensesEntries.add(0.0);employeeExpensesEntries.add(0.0);employeeExpensesEntries.add(0.0);employeeExpensesEntries.add(0.0);employeeExpensesEntries.add(0.0);employeeExpensesEntries.add(0.0);
-			builderdefaultWeekEntry.setEmployeeExpenses(employeeExpensesEntries);
-			List<Double> hoursEntries = new ArrayList<Double>();
-			
-			hoursEntries.add(0.0);hoursEntries.add(0.0);hoursEntries.add(0.0);hoursEntries.add(0.0);hoursEntries.add(0.0);hoursEntries.add(0.0);hoursEntries.add(0.0);
-			builderdefaultWeekEntry.setEmployeeHours(hoursEntries);
-			
-			repositoryWeekEntry.addEmployeeWeekEntry(user, builderdefaultWeekEntry.getPeriodPayEntry());
 		}
 	}
 
 	@Override
-	public void updateUser(UserEntry user)
-	{
-		
-		if (getUserByEmail(user.getEmail()) != null){
-			
+	public void updateUser(UserEntry user) {
+
+		if (getUserByEmail(user.getEmail()) != null) {
+
 			ArrayList<String> userelement = getUserString(user);
 			xmluserpersistance.updateUser(userelement);
 		}
@@ -156,44 +137,42 @@ public class UserRepositoryImpl implements UserRepository
 	}
 
 	@Override
-	public void deleteUser(UserEntry user)
-	{
-		
-		if (getUserByEmail(user.getEmail()) != null){
-		
-			xmluserpersistance.deleteUser(user.getEmail());	
-			
+	public void deleteUser(UserEntry user) {
+
+		if (getUserByEmail(user.getEmail()) != null) {
+
+			xmluserpersistance.deleteUser(user.getEmail());
+
 			WeekEntryRepository repositoryWeekEntry = new WeekEntryRepositoryImpl();
-			ArrayList<WeekEntry> listEmployeeWeekEntry = repositoryWeekEntry.getWeekEntryByEmail(user.getEmail());
-			for(WeekEntry weekEntry: listEmployeeWeekEntry){
+			ArrayList<WeekEntry> listEmployeeWeekEntry = repositoryWeekEntry
+					.getWeekEntryByEmail(user.getEmail());
+			for (WeekEntry weekEntry : listEmployeeWeekEntry) {
 				repositoryWeekEntry.deleteWeekEntry(weekEntry);
 			}
 		}
 	}
 
 	@Override
-	public void addTaskToUser(TaskEntry task, UserEntry user)
-	{
-		
-		if (getUserByEmail(user.getEmail()) != null){
+	public void addTaskToUser(TaskEntry task, UserEntry user) {
+
+		if (getUserByEmail(user.getEmail()) != null) {
 
 			ArrayList<String> userElement = getUserString(user);
 			ArrayList<String> taskElement = getTaskString(task);
 			xmluserpersistance.addTaskToUser(taskElement, userElement);
 		}
-		
+
 	}
 
 	@Override
-	public void setTasksToUser(List<TaskEntry> tasks, UserEntry user)
-	{
+	public void setTasksToUser(List<TaskEntry> tasks, UserEntry user) {
 		ArrayList<ArrayList<String>> tasksElements = new ArrayList<ArrayList<String>>();
-		
-		if (getUserByEmail(user.getEmail()) != null){
-		
+
+		if (getUserByEmail(user.getEmail()) != null) {
+
 			ArrayList<String> userElement = getUserString(user);
-			
-			for(TaskEntry task : tasks){
+
+			for (TaskEntry task : tasks) {
 				ArrayList<String> taskElement = getTaskString(task);
 				tasksElements.add(taskElement);
 			}
@@ -201,22 +180,20 @@ public class UserRepositoryImpl implements UserRepository
 		}
 	}
 
-
 	private ArrayList<String> getUserString(UserEntry user) {
 		ArrayList<String> userElement = new ArrayList<String>();
-		
+
 		userElement.add(0, user.getName());
 		userElement.add(1, user.getEmail());
 		userElement.add(2, user.getRole().toString());
 		userElement.add(3, user.getHashedPassword());
-				
-		
+
 		if (user instanceof EmployeeEntry) {
-			
-			userElement.add(4, ((EmployeeEntry)user).getCompany());
-			userElement.add(5, ((EmployeeEntry)user).getDepartment());
-			userElement.add(6, ((EmployeeEntry)user).getRateHour().toString());
-			
+
+			userElement.add(4, ((EmployeeEntry) user).getCompany());
+			userElement.add(5, ((EmployeeEntry) user).getDepartment());
+			userElement.add(6, ((EmployeeEntry) user).getRateHour().toString());
+
 			if (((EmployeeEntry) user).getTasks().size() > 0) {
 				for (int i = 7; i < ((EmployeeEntry) user).getTasks().size(); i++) {
 					TaskEntry task = ((EmployeeEntry) user).getTasks().get(i);
@@ -224,16 +201,17 @@ public class UserRepositoryImpl implements UserRepository
 				}
 			}
 		}
-		
+
 		return userElement;
 	}
-	
-	private ArrayList<String> getTaskString(TaskEntry task){
+
+	private ArrayList<String> getTaskString(TaskEntry task) {
 		ArrayList<String> taskElement = new ArrayList<String>();
-		
+
 		taskElement.add(0, task.getId().toString());
-		taskElement.add(1, task.getName());;
-		
+		taskElement.add(1, task.getName());
+		;
+
 		return taskElement;
 	}
 }
